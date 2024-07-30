09bd066fe382327c2da7d072c2d5ea4a5685e8c6
Franklin Azriel <franklinazriel6942@outlook.com>
2024-07-30 21:09:23 +0700

    build: Copy recovery folder into ramdisk

--- device-common.mk
+++ device-common.mk
@@ -10,3 +10,6 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Copy recovery folder
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
